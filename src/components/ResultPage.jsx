c009a6792262f9e984d24136e674596aaf0dc31c
reinexxism <lucy0805@naver.com>
2024-01-15 22:07:58 +0900

    :art: 테스트 초기 화면으로 돌아가기 완성
    계속 왜 안되나 했다

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ResultPage() {
   return (
     <div>
       <h1>여기는 결과 페이지입니다.</h1>
       <p>당신의 결과는?</p>
-      <button>다시 하기</button>
+      <button>
+        <Link to="/question/1">테스트 다시하기</Link>
+      </button>
     </div>
   );
 }
